5b7f74ec0b535986d2344c7de8ea75747aedeeee
BaseEditor <145483776+BaseEditor@users.noreply.github.com>
2023-12-07 16:40:21 -0800

    Updated assignment B4 code

--- Assignment B4/app.R
+++ Assignment B4/app.R
@@ -7,16 +7,25 @@ library(tidyverse)
 library(bslib)
 
 
-# UI for application that draws two histograms and two tables
+# UI for application that draws a histogram and table
+# Artwork was added for assignment b4 in order to improve the aesthetics of the page, as well as give a visual on the
+#different species and what the bill measurements refer to
 ui <- fluidPage(
   theme = bs_theme(bootswatch = "darkly"),
-  titlePanel("Penguin Body Mass App"),
-  h4("Use this app to investigate penguin body mass (g) and bill length (mm) in relation to flipper length
+  titlePanel("Palmer Penguins Analysis"),
+  h4("Use this app to investigate physical penguin measurements in relation to flipper length
   (mm), across three different species"),
   sidebarLayout(
     sidebarPanel(
       img(src = "palmerpenguins.png", style="display: block; margin-left: auto; margin-right: auto;",
           height = 200, width = 200),
+      selectInput("variable", "Graphed Variable",
+                  c("Body mass (g)" = "body_mass_g",
+                    "Bill length (mm)" = "bill_length_mm",
+                    "Bill Depth (mm)" = "bill_depth_mm"
+                  )
+        
+      ),
       sliderInput("Flipper_Slider", "Select penguin flipper length range (mm)", min = 172, max = 231,
               value = c(190, 210)),
       checkboxGroupInput(inputId = "Species_Select", "Select penguin species", 
@@ -29,21 +38,25 @@ ui <- fluidPage(
       ),
     mainPanel(
       plotOutput("BodyMass"),
+      h6("Body Mass (g) Summary Statistic Table"),
       tableOutput("BodyMassTable"),
-      plotOutput("BillLength"),
-      tableOutput("BillLengthTable")
     )
   ),
   h6("Artwork by @allison_horst")
 )
 
-# Code for filtering slider and selection check boxes, two histograms, and two tables
+# Code for variable selection, filtering slider, and selection check boxes, histogram, and table
 server <- function(input, output) 
   {
+#A variable selection tool was added for assignment B4, to allow the user to specify which variable they
+#would like plotted on the histogram
+  observe(print(input$variable))
   observe(print(input$Flipper_Slider))
   observe(print(input$Species_Select))
   
 #Filter by species
+#The feature of filtering by species, to allow users to select which species they would like to plot data
+#from has been added for assignment B4
   penguinsSpecies <- reactive({
     penguins %>%
       dplyr::filter(
@@ -61,38 +74,21 @@ server <- function(input, output)
   })
   
 
-#Body mass histogram
+#Histogram
   output$BodyMass <- renderPlot({
     penguinsFiltered() %>%
-    ggplot(aes(body_mass_g)) +
+    ggplot(aes_string(input$variable)) +
       geom_histogram(aes(fill = species)) +
-      ggtitle("Body Mass Distribution") +
-      xlab("Penguin Body Mass (g)") +
+      ggtitle("Distribution of Selected Variable") +
+      xlab("Selected Variable") +
       theme_minimal()
   })
 
-#Body mass table
+#Body Mass Table
   output$BodyMassTable <- renderTable({
     penguinsFiltered() %>%
       summarise(Mean = mean(body_mass_g, na.rm = TRUE), 
       Min = min(body_mass_g, na.rm = TRUE), Max = max(body_mass_g, na.rm = TRUE))
-  })
-  
-#Bill length histogram
-  output$BillLength <- renderPlot({
-    penguinsFiltered() %>%
-      ggplot(aes(bill_length_mm)) +
-      geom_histogram(aes(fill = species)) +
-      ggtitle("Bill Length Distribution") +
-      xlab("Penguin Bill Length (mm)") +
-      theme_minimal()
-  })
-  
-#Bill length table
-  output$BillLengthTable <- renderTable({
-    penguinsFiltered() %>%
-      summarise(Mean = mean(bill_length_mm, na.rm = TRUE), 
-                Min = min(bill_length_mm, na.rm = TRUE), Max = max(bill_length_mm, na.rm = TRUE))
   })
 }
 
